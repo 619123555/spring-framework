c783aacefe10f831651bde365bdf7c129164066a
619123555 <619123555@qq.com>
2021-02-02 16:53:38 +0800

    debug模块提交测试类

--- spring-debug/src/main/java/org/springframework/debug/applicationContext/MyApplicationContext.java
+++ spring-debug/src/main/java/org/springframework/debug/applicationContext/MyApplicationContext.java
@@ -1,4 +1,4 @@
-package org.springframework.debug;
+package org.springframework.debug.applicationContext;
 
 import org.springframework.beans.BeansException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -12,5 +12,13 @@ public class MyApplicationContext extends ClassPathXmlApplicationContext {
 
 	public MyApplicationContext(String... configLocations) throws BeansException {
 		super(configLocations);
+	}
+
+
+	// 获取应用系统属性或系统环境信息
+	@Override
+	protected void initPropertySources() {
+		System.out.println(getEnvironment().getPropertySources().get("systemProperties").getProperty("user.name"));
+		super.initPropertySources();
 	}
 }
---
+++ spring-debug/src/main/java/org/springframework/debug/applicationContext/TestObject.java
@@ -0,0 +1,11 @@
+package org.springframework.debug.applicationContext;
+
+/**
+ * @auther: 胡鹏祥
+ * @date: 2021/1/28
+ * @description:
+ */
+public class TestObject {
+	private String name;
+	private Integer age;
+}
